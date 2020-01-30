fe80aca98d43e32e9f4af883eb99639628d9cf9d
gaborTamasi <gabor.tamasi90@gmail.com>
2020-01-30 18:15:52 +0100

    rendezvenyek oldal modositva

---
+++ js/event-descriptions.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+    SelectCategory();  
+      
+});
+
+function SelectCategory(){
+    var classForShow = "selected";
+
+    $("li[data-event]").click(function(){
+
+        var btnData = $(this).data("event");
+        $("li[data-event]").removeClass(classForShow);
+        $(this).addClass(classForShow);               
+        
+        $("[data-event]").removeClass(classForShow);
+        $("[data-event='" + btnData +  "']").first().addClass(classForShow);
+    });
+}  
