5e08d4077f3e09df38c825500925e0af6638bc01
gaborTamasi <gabor.tamasi90@gmail.com>
2020-03-05 18:02:18 +0100

    header fejlesztese / email elrejtese

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -2,18 +2,37 @@ $(document).ready(function(){
 
     $(window).scroll(function(){
         slideDownHeader($(".nav-container"));
-        console.log($(document).scrollTop());
     });
 
     
-
+// Sticks navigation to top on scrolling down
     function slideDownHeader(navContainer){
         if($("body").scrollTop() > 200 || $(document).scrollTop() > 200){
             $(navContainer).addClass("sticky");
-            console.log("fired");
         } else{
             $(navContainer).removeClass("sticky");
         }
     }
 
 });
+
+// Shows email address with a small amount of delay upon clicking the button
+
+$(document).ready(function(){
+
+    var emailContainer = $(".email-address-container");
+    var btn = $(".show-mail-btn");
+    var loadingText = '<p class="loadingText">(Kis türelmet kérünk...)</p>'
+
+    $(emailContainer).append(loadingText);
+
+    setTimeout(function(){        
+        $('.loadingText').remove();
+        btn.removeClass("hidden");
+        $(btn).click(function(){
+            $(this).css('display', 'none');
+            $(emailContainer).append("<h3><strong>info@kisfaludygozos.hu</strong></h3>");
+        });
+    }, 1200);
+   
+})
