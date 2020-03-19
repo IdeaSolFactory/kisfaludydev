c44ce2aab99330acc29ce252cabe4fdbf2e3d06a
gaborTamasi <gabor.tamasi90@gmail.com>
2020-03-19 15:07:38 +0100

    navbar dropdown / toggle script

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -4,22 +4,39 @@ $(document).ready(function(){
         slideDownHeader($(".nav-container"));
     });
 
-    
-// Sticks navigation to top on scrolling down
-    function slideDownHeader(navContainer){
-        if($("body").scrollTop() > 200 || $(document).scrollTop() > 200){
-            $(navContainer).addClass("sticky");
-        } else{
-            $(navContainer).removeClass("sticky");
-        }
-    }
+    toggleNavBackground();
+    showEmailDelay();   
 
 });
 
+// Sticks navigation to top on scrolling down
+function slideDownHeader(navContainer){
+    if($("body").scrollTop() > 200 || $(document).scrollTop() > 200){
+        $(navContainer).addClass("sticky");
+    } else{
+        $(navContainer).removeClass("sticky");
+    }
+}
+
+// Toggles sticky menu upon clicking on hamburger menu at the top
+function toggleNavBackground(){
+
+    var hamburgerMenu = $(".hamburger-toggle");
+    var navBar = $(".nav-container");
+    var stickyClass = "sticky";
+
+    $(hamburgerMenu).click(function(){
+
+        if( !$(navBar).hasClass("sticky")){
+            $(navBar).addClass("sticky");
+        }
+    });
+
+};
+
 // Shows email address with a small amount of delay upon clicking the button
 
-$(document).ready(function(){
-
+function showEmailDelay(){
     var emailContainer = $(".email-address-container");
     var btn = $(".show-mail-btn");
     var loadingText = '<p class="loadingText">(Kis türelmet kérünk...)</p>'
@@ -34,5 +51,4 @@ $(document).ready(function(){
             $(emailContainer).append("<h3><strong>info@kisfaludygozos.hu</strong></h3>");
         });
     }, 1200);
-   
-})
+};
