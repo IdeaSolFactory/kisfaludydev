6cb2b34ed32348e29349418d1ceacbb02d16d8b8
gaborTamasi <gabor.tamasi90@gmail.com>
2020-03-25 10:01:00 +0100

    Revert "Merge branch 'develop' into tervrajz-szekcio"
    This reverts commit 695c98e835c8b222f75c85a40d3b291579abd11e, reversing
    changes made to 62891eb5a02df7dea1628ed13488269967e9bb55.

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -2,82 +2,18 @@ $(document).ready(function(){
 
     $(window).scroll(function(){
         slideDownHeader($(".nav-container"));
+        console.log($(document).scrollTop());
     });
 
-    toggleNav();
-    showEmailDelay();
-    contentToggle(); 
+    
+
+    function slideDownHeader(navContainer){
+        if($("body").scrollTop() > 200 || $(document).scrollTop() > 200){
+            $(navContainer).addClass("sticky");
+            console.log("fired");
+        } else{
+            $(navContainer).removeClass("sticky");
+        }
+    }
 
 });
-
-// Content Toggle
-function contentToggle(){
-    var showClass = "show";
-    var initBtnText = "Több";
-    var activeBtnText = "Kevesebb";
-
-    $(".content-reveal__btn").click(function(){
-        var buttonClicked = $(this);
-        var contentToReveal = $(buttonClicked).siblings(".hidden-content");
-
-        if( !$(contentToReveal).hasClass(showClass) ){
-            $(contentToReveal).addClass(showClass);
-            $(buttonClicked).html(activeBtnText);
-        } else{
-            $(contentToReveal).removeClass(showClass);
-            $(buttonClicked).html(initBtnText);
-        }
-    });
-}
-
-// Sticks navigation to top on scrolling down
-function slideDownHeader(navContainer){
-    if($("body").scrollTop() > 200 || $(document).scrollTop() > 200){
-        $(navContainer).addClass("sticky");
-    } else{
-        $(navContainer).removeClass("sticky");
-    }
-}
-
-// Toggles sticky menu upon clicking on hamburger menu at the top
-function toggleNav(){
-
-    var hamburgerMenu = $(".hamburger-toggle");
-    var navBar = $(".nav-container");
-    var navBarMenu = $("#menu1");
-    var stickyClass = "sticky";
-    var menuActive = "active";
-
-    $(hamburgerMenu).click(function(){
-
-        if( !$(navBarMenu).hasClass(menuActive) ){
-            $(navBarMenu).addClass(menuActive);
-        } else{
-            $(navBarMenu).removeClass(menuActive);
-        }
-
-        if( !$(navBar).hasClass(stickyClass)){
-            $(navBar).addClass(stickyClass);
-        }
-    });
-
-};
-
-// Shows email address with a small amount of delay upon clicking the button
-
-function showEmailDelay(){
-    var emailContainer = $(".email-address-container");
-    var btn = $(".show-mail-btn");
-    var loadingText = '<p class="loadingText">(Kis türelmet kérünk...)</p>'
-
-    $(emailContainer).append(loadingText);
-
-    setTimeout(function(){        
-        $('.loadingText').remove();
-        btn.removeClass("hidden");
-        $(btn).click(function(){
-            $(this).css('display', 'none');
-            $(emailContainer).append("<h3><strong>info@kisfaludygozos.hu</strong></h3>");
-        });
-    }, 1200);
-};
